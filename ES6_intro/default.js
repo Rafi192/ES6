d333a261652c964adcf4f202dac12d2b2af69db6
Rafi192 <rafihasanmmsc@gmail.com>
2025-09-07 19:26:31 +0600

    Commit of template string ES6

---
+++ ES6_intro/default.js
@@ -0,0 +1,8 @@
+function sum(num1,num2=9){
+    const total = num1 + num2;
+    console.log(num1, num2, total);
+}
+
+// sum(10,20, 40,50 ,60);
+// default parameter = undefined
+sum(10)
---
+++ ES6_intro/templateString.js
@@ -0,0 +1,8 @@
+const poem ="this is a normal string"
+
+const string2 = 'this is a one line string'
+
+const tempString = `this is a template 
+string example ${string2}`;
+
+console.log(tempString)
