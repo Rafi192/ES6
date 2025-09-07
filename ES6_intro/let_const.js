f5264504e5d8ac4732d3d0e8553edcbba556b4bc
Rafi192 <rafihasanmmsc@gmail.com>
2025-09-07 15:00:20 +0600

    Commit of let vs const in es6

---
+++ ES6_intro/let_const.js
@@ -0,0 +1,34 @@
+const name = 'Bangladesh';
+// name = 'bd';//Const variables can not be modified
+
+// In JavaScript, 
+// const does not mean immutable (unchangeable).
+
+// It only means: 
+// 👉 You cannot reassign the variable to a new value.
+
+let price = 213123;
+price = 222; // let variables cab be modified.
+
+console.log(price);
+
+// console.log(device)
+var device = 'laptop'
+
+
+// Objects and Arrays
+
+// Objects and arrays are reference types.
+
+// A const variable
+//  that holds an object/array cannot be reassigned to a new object/array,
+//  but the contents of the object/array can still be modified.
+
+const obj = {name:'hasan', age:23, country:'UAE'}
+
+obj.city ='Dubai';
+console.log(obj)
+
+Object.assign(obj, {hobby:"coding"})
+
+console.log(obj)
